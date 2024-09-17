c332bc705fd7262599411f0654c3cc24d801cf25
REDFLAG-bugs <eee.21beeb21@silicon.ac.in>
2024-09-17 18:11:14 +0530

    updated utilities

---
+++ send-email.php
@@ -0,0 +1,29 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $firstName = htmlspecialchars($_POST['firstName']);
+    $lastName = htmlspecialchars($_POST['lastName']);
+    $countryCode = htmlspecialchars($_POST['countryCode']);
+    $phone = htmlspecialchars($_POST['phone']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "anshumanmishra274@gmail.com";
+    $subject = "New Contact Form Submission";
+    $headers = "From: " . $email . "\r\n";
+    $headers .= "Reply-To: " . $email . "\r\n";
+
+    $body = "First Name: $firstName\n";
+    $body .= "Last Name: $lastName\n";
+    $body .= "Phone: $countryCode $phone\n";
+    $body .= "Email: $email\n";
+    $body .= "Message:\n$message\n";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "success";
+    } else {
+        echo "error";
+    }
+} else {
+    echo "error";
+}
+?>
